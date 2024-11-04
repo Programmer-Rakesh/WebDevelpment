9fe9ba1f8e7cf07c6fb75fb024aaef895c53c5e3
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-11-04 23:29:34 +0530

    Done for today

--- js/part2/02_nonPrimitives.js
+++ js/part2/02_nonPrimitives.js
@@ -1,3 +1,29 @@
-let obj = {}
-console.log(obj)
-console.log(typeof obj)
+const username = {
+    firstname: "rakesh",
+    "middle name" : "nil",
+    isLoggedin: true,
+};
+
+username.firstname = "hapsi"
+username.lastname = "okay"
+
+console.log(username.firstname);
+console.log(username["middle name"]);
+console.log(username.lastname);
+console.log(username);
+console.log(typeof username);
+
+let today = new Date();
+console.log(today.getDate());   // 4 
+
+let anotherUser = ["rakesh", true];
+console.log(anotherUser[0]);    // Will print rakesh
+
+let isValue = true;  // JS consider true as 1 & false as 0
+console.log(1 + "1");   // will print 11
+console.log(isValue + 1);   // Will print 2
+
+
+let isValue1 = "2abc";
+console.log(typeof Number(isValue1));   // It will print "Number"
+console.log(Number(null));
