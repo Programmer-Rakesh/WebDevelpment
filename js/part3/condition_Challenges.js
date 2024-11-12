9d9ef4a99e298d033b10780e47f88cbbf4ff2935
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-11-12 23:08:00 +0530

    Done for today

---
+++ js/part3/condition_Challenges.js
@@ -0,0 +1,31 @@
+/*Checking if a number is greater than another number:
+
+let a = 5;
+let b = 7;
+
+if (a < b){
+    console.log("Hell yeahh!!")
+}
+else {
+    console.log("Nope")
+}*/
+
+
+
+
+/* checking if a string is eual to another string:
+
+let username = "Rakesh"
+let anotherUsername = "Boii"
+
+if (username != anotherUsername){
+    console.log("Nauthy naughty")
+}
+else{
+    console.log("noy way")
+}*/
+
+
+
+
+/*checking if a variable is a number or not*/
