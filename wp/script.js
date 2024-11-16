db4c839671c151ae88a22f453531c4e472ff72a0
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-11-17 02:45:45 +0530

    Done for today

--- wp/script.js
+++ wp/script.js
@@ -1,15 +1,23 @@
-function yeah() {
-        let Okay = document.getElementById("a").value;
+function search() {
+        let okay = document.getElementById("a").value;
         let f = document.getElementById("f");
-        let pp = document.getElementById("umm")
+        let pp = document.getElementById("um")
+        let ww = document.getElementById("op")
+        let vg = document.getElementById("ou")
+        let vp = document.getElementById("ot")
+        let vs = document.getElementById("os")
 
         let img = document.createElement("img");
         img.width = 250;
         
 
-            if ( Okay<12){
-                f.value ="B1";
+            if ( okay==12  ||  okay==11){
+                f.value = "Rakesh Roy";
                 img.src = "./images/B1 lancer.jpg"
+                ww.value = "Roll no. : 24SECE130051"
+                vg.value = "Date of Joining : 19/08/2024"
+                vp.value = "Course : BTech ECE"
+                vs.value = "Specialisation : AI & ML"
             }
             else{
                 f.value = "B2";
