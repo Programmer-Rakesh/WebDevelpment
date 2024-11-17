6231cfdd94c8fb3514e2827de34af542111f1f65
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-11-17 16:00:42 +0530

    Done

--- wp/script.js
+++ wp/script.js
@@ -8,20 +8,32 @@ function search() {
         let vs = document.getElementById("os")
 
         let img = document.createElement("img");
-        img.width = 250;
+        img.width = 160;
         
 
-            if ( okay==12  ||  okay==11){
+            if ( okay==="24SECE1030051" ||  okay==="51"  || okay==="Rakesh Roy" || okay==="rakesh"  || okay==="Rakesh"){
                 f.value = "Rakesh Roy";
-                img.src = "./images/B1 lancer.jpg"
-                ww.value = "Roll no. : 24SECE130051"
+                img.src = "./images/pp.jpg"
+                ww.value = "Roll no : 24SECE130051"
                 vg.value = "Date of Joining : 19/08/2024"
                 vp.value = "Course : BTech ECE"
                 vs.value = "Specialisation : AI & ML"
             }
+            else if ( okay==="24SECE1030020" ||  okay==="20"  || okay==="Piyush kumar singh" || okay === "Piyush"  || okay==="Piyush kumar" || okay ==="piyush"){
+                f.value = "Rakesh Roy";
+                img.src = "./images/pp2.jpg"
+                ww.value = "Roll no : 24SECE130020"
+                vg.value = "Date of Joining : 25/08/2024"
+                vp.value = "Course : BTech CSE"
+                vs.value = "Specialisation : AI"
+            }
             else{
                 f.value = "B2";
                 img.src ="./images/B2 spirit.jpg"
+                ww.value = "Roll no : 24SECE130051"
+                vg.value = "Date of Joining : 25/08/2024"
+                vp.value = "Course : BTech CSE"
+                vs.value = "Specialisation : AI & ML"
             }  
 
             // img.onload = function () { 
